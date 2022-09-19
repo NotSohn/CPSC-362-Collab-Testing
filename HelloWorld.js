e8031735f1817876c074a8bacbe2b972014e8b85
cjacobsencsuf <carlajacobsen@gmail.com>
2022-09-19 13:36:10 -0700

    hello im carla i am doing my commit

--- HelloWorld.js
+++ HelloWorld.js
@@ -14,24 +14,24 @@ console.log(
 
     Product Owner:
     Scrum Master:
-    QA (Tester): 
+    QA (Tester):  CArla Jacobsen
     Tech Chaser:
     Tech Lead: Justin Sohn
 
 
-    it should be 
-    
+    it should be
+
     'git add ./HelloWorld.js'
 
-    'git commit -m "intial commit" <-- Only writing this because this is the initial commit of the file, when commit this file again state what you changed -Justin 
+    'git commit -m "intial commit" <-- Only writing this because this is the initial commit of the file, when commit this file again state what you changed -Justin
     ('git status' to make sure that the file you commited is commited)
 
         EXAMPLE(This is how my terminal in wsl looks like): justin@DESKTOP-UT6G8TD:/mnt/c/Users/Justin Sohn/Github Repositories/CPSC-362-Collab-Testing$ git status
                                                             On branch main
                                                             Your branch is ahead of 'origin/main' by 1 commit.
                                                             (use "git push" to publish your local commits)
-    
-    
+
+
     'git push origin main'
 
     * If you're doing this thru wsl then you kinda have to go through a process to git push so i'm going to leave a link here for whoever is using wsl https://stackoverflow.com/questions/68775869/message-support-for-password-authentication-was-removed-please-use-a-personal
